b3bd5499dbc20c8a507e7c96d37b4ade1c0e24c8
marinostheo <marinostheo@yahoo.gr>
2019-11-27 22:34:54 +0200

    Updated index.php
    Put everything inside a newly created div mainContainer, created div topContainer and inside it the div navBarContainer which contains all the elements of the navbar that appears on the left side of the page.

--- index.php
+++ index.php
@@ -21,68 +21,134 @@ else {
 
 <body>
 
-    <div id="nowPlayingBarContainer">
+    <div id="mainContainer">
 
-        <div id="nowPlayingBar">
+        <div id="topContainer">
 
-            <div id="nowPlayingLeft">
-
-                <div class="content">
-                    
-                    <span class="albumLink">
-                        <img src="http://clipart-library.com/img/2008830.jpg" class="albumArtwork">
-                    </span>
-
-                    <div class="trackInfo">
-                        
-                        <span class="trackName">
-                            <span>Happy Birthday</span>
-                        </span>
-
-                        <span class="artistName">
-                            <span>Marinos Theo</span>
-                        </span>
-                    
-                    </div>
-                </div>
+            <div id="navBarContainer">
             
+                <nav class="navBar">
+                
+                    <a href="index.php" class="logo">
+                        <img src="assets/images/icons/logo.png">
+                    </a>
+
+                    <div class="group">
+                    
+                        <div class=navItem>
+                            <a href="search.php" class="navItemLink">Search
+                                <img src="assets/images/icons/search.png" class="icon" alt="Search">
+                            </a>
+                        </div>
+                    
+                    </div>
+
+                    <div class="group">
+                    
+                        <div class=navItem>
+                            <a href="browse.php" class="navItemLink">Browse</a>
+                        </div>
+
+                        <div class=navItem>
+                            <a href="yourMusic.php" class="navItemLink">Your Music</a>
+                        </div>
+
+                        <div class=navItem>
+                            <a href="profile.php" class="navItemLink">Marinos Theo</a>
+                        </div>
+                    
+                    </div>
+                
+                </nav>
+
             </div>
+        
+        </div>
 
-            <div id="nowPlayingCenter">
+        <div id="nowPlayingBarContainer">
 
-                <div class="content playerControls">
+            <div id="nowPlayingBar">
 
-                    <div class="buttons">
+                <div id="nowPlayingLeft">
 
-                        <button class="controlButton shuffle" title="Shuffle button">
-                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-                        </button>
+                    <div class="content">
+                        
+                        <span class="albumLink">
+                            <img src="http://clipart-library.com/img/2008830.jpg" class="albumArtwork">
+                        </span>
 
-                        <button class="controlButton previous" title="Previous button">
-                            <img src="assets/images/icons/previous.png" alt="Previous">
-                        </button>
+                        <div class="trackInfo">
+                            
+                            <span class="trackName">
+                                <span>Happy Birthday</span>
+                            </span>
 
-                        <button class="controlButton play" title="Play button">
-                            <img src="assets/images/icons/play.png" alt="Play">
-                        </button>
+                            <span class="artistName">
+                                <span>Marinos Theo</span>
+                            </span>
+                        
+                        </div>
+                    </div>
+                
+                </div>
 
-                        <button class="controlButton pause" title="Pause button" style="display: none">
-                            <img src="assets/images/icons/pause.png" alt="Pause">
-                        </button>
+                <div id="nowPlayingCenter">
 
-                        <button class="controlButton next" title="Next button">
-                            <img src="assets/images/icons/next.png" alt="Next">
-                        </button>
+                    <div class="content playerControls">
 
-                        <button class="controlButton repeat" title="Repeat button">
-                            <img src="assets/images/icons/repeat.png" alt="Repeat">
-                        </button>
+                        <div class="buttons">
+
+                            <button class="controlButton shuffle" title="Shuffle button">
+                                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                            </button>
+
+                            <button class="controlButton previous" title="Previous button">
+                                <img src="assets/images/icons/previous.png" alt="Previous">
+                            </button>
+
+                            <button class="controlButton play" title="Play button">
+                                <img src="assets/images/icons/play.png" alt="Play">
+                            </button>
+
+                            <button class="controlButton pause" title="Pause button" style="display: none">
+                                <img src="assets/images/icons/pause.png" alt="Pause">
+                            </button>
+
+                            <button class="controlButton next" title="Next button">
+                                <img src="assets/images/icons/next.png" alt="Next">
+                            </button>
+
+                            <button class="controlButton repeat" title="Repeat button">
+                                <img src="assets/images/icons/repeat.png" alt="Repeat">
+                            </button>
+
+                        </div>
+
+                        <div class="playbackBar">
+                            
+                            <span class="progressTime current">0.00</span>
+
+                            <div class="progressBar">
+                                <div class="progressBarBg">
+                                    <div class="progress"></div>
+                                </div>
+                            </div>
+
+                            <span class="progressTime remaining">0.00</span>
+
+                        </div>
 
                     </div>
 
-                    <div class="playbackBar">
-                        
-                        <span class="progressTime current">0.00</span>
+                </div>
+
+                <div id="nowPlayingRight">
+
+                    <div class="volumeBar">
+
+                        <button class="controlButton volume" title="Volume button">
+                            <img src="assets/images/icons/volume.png" alt="Volume">
+                        </button>
 
                         <div class="progressBar">
                             <div class="progressBarBg">
@@ -90,26 +156,6 @@ else {
                             </div>
                         </div>
 
-                        <span class="progressTime remaining">0.00</span>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div id="nowPlayingRight">
-
-                <div class="volumeBar">
-
-                    <button class="controlButton volume" title="Volume button">
-                        <img src="assets/images/icons/volume.png" alt="Volume">
-                    </button>
-
-                    <div class="progressBar">
-                        <div class="progressBarBg">
-                            <div class="progress"></div>
-                        </div>
                     </div>
 
                 </div>
