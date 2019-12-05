5d99adca3e5dc48533516ee936eef4955c2e5f35
marinostheo <marinostheo@yahoo.gr>
2019-12-05 21:19:43 +0200

    Updated nowPlayingBar.php
    Added to the playSong function an ajax call to update the plays count of songs.

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -35,8 +35,8 @@ function setTrack(trackId, newPlaylist, play) {
             $(".albumLink img").attr("src", album.artworkPath);
         });
 
-        audioElement.setTrack(track.path);
-        audioElement.play();
+        audioElement.setTrack(track);
+        playSong();
     });
 
     if(play == true) {
@@ -45,6 +45,11 @@ function setTrack(trackId, newPlaylist, play) {
 }
 
 function playSong() {
+
+    if(audioElement.audio.currentTime == 0) {
+        $.post("includes/handlers/ajax/updatePlays.php", { songId: audioElement.currentlyPlaying.id });
+    }
+
     $(".controlButton.play").hide();
     $(".controlButton.pause").show();
     audioElement.play();
